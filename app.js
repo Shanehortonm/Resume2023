ab3e021e2a8e30dc480dfcf27bb478d78d8046bc
Shane Horton <shanehortonm@gmail.com>
2023-01-12 18:41:38 -0500

    overall changes, added experience section, work on contact section

--- app.js
+++ app.js
@@ -8,9 +8,26 @@ Hamburger Menu
 const menu = document.querySelector('#mobile-nav');
 const menuLinks = document.querySelector('.navbar-menu');
 
+
+
+
 const hamburgerMenu = () => {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 };
 
 menu.addEventListener('click', hamburgerMenu);
+
+
+
+
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active');
+    if (window.innerWidth <= 768 && menuBars) {
+      menu.classList.toggle('is-active');
+      menuLinks.classList.remove('active');
+    }
+  };
+  
+  menuLinks.addEventListener('click', hideMobileMenu);
+  navLogo.addEventListener('click', hideMobileMenu);
