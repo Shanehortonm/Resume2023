63cc6be96b83910671a15699821a4e9478e2a71f
Shanehortonm <shanehortonm@gmail.com>
2023-01-25 13:49:05 -0500

    work on email, use of email js

--- app.js
+++ app.js
@@ -19,8 +19,6 @@ const hamburgerMenu = () => {
 menu.addEventListener('click', hamburgerMenu);
 
 
-
-
 const hideMobileMenu = () => {
     const menuBars = document.querySelector('.is-active');
     if (window.innerWidth <= 768 && menuBars) {
@@ -31,3 +29,46 @@ const hideMobileMenu = () => {
   
   menuLinks.addEventListener('click', hideMobileMenu);
   navLogo.addEventListener('click', hideMobileMenu);
+
+
+
+/*
+==================================================
+Email PopUp
+==================================================
+*/
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+  
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+/*
+==================================================
+Email link to Email.js
+==================================================
+*/
+
+let sendBtn = document.getElementById('sendBtn');
+const form = document.getElementById('form');
+
+sendBtn.addEventListener('click', function(e) {
+	e.preventDefault();
+
+	sendBtn.value = 'Sending...';
+
+	const serviceID = 'default_service';
+  const templateID = 'template_94od5g2';
+
+  emailjs.sendForm(serviceID, templateID, form)
+    .then(() => {
+      sendBtn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      sendBtn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+})
